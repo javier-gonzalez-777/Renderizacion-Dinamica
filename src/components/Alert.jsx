ac6607761e0d8daba751ee2efec19a35dfc0df4d
Luccrivano <lucciano.rivano@gmail.com>
2024-02-25 00:56:31 -0300

    Add color Alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
-
-
-const Alert = ({error}) => {
+const Alert = ({ error }) => {
   return (
     <>
-       {/* {console.log(mensaje)} */}
-       {error ? <p  className="error">Completa todos los campos</p> :<p className="error">El Regidtro fue exitoso</p> }
-      
-
-    
+      {/* {console.log(mensaje)} */}
+      {error ? (
+        <p className="error">Completa todos los campos</p>
+      ) : (
+        <p className="error">El Regidtro fue exitoso</p>
+      )}
     </>
-  )
-}
+  );
 
-export default Alert
+  return mensaje.includes("agregado") || mensaje.includes("eliminado")
+    ? "alert bg-info"
+    : "alert bg-danger";
+};
+
+export default Alert;
